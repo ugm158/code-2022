2198dd9c8e791b352c017a89dfafcca8f716f6a5
ugm158 <ugm158@gmail.com>
2022-12-22 17:02:16 +0800

    20221222-7-3

--- gas-2022-1222-7.js
+++ gas-2022-1222-7.js
@@ -1,5 +1,84 @@
 
 /*=====================================
+  20221222-7 內容
+=====================================*/
+function test1(e){
+  //取得試算表
+  let ss = SpreadsheetApp.getActiveSpreadsheet();
+  //取得工作表
+  let ws = ss.getSheetByName('工作表7');
+  //取得範圍
+  let range;
+
+  e = {
+    "parameter":
+      {"date":"2022-12-23",
+       "money":"1000",
+       "ps":"1600",
+       "unit":"台新",
+       "callback_url":
+       "http://127.0.0.1:5500/form_20221222.html",
+       "op":"payment"}
+  }
+
+  // console.log(e.parameter.unit);
+  console.log(e['parameter']['unit']);
+
+  let stru = [{
+    label: '繳費單位',
+    type: 'select',
+    name: 'unit',
+    value: '',
+    valid: '', //required
+    option: '玉山|國泰|台新|電費|水費',
+    width: '3'
+  },
+  {
+    label: '繳費日期',
+    type: 'date',
+    name: 'date',
+    value: '',
+    valid: 'required', //required
+    option: '',
+    width: '3'
+  },
+  {
+    label: '繳費金額',
+    type: 'text',
+    name: 'money',
+    value: '',
+    valid: 'required', //required
+    option: '',
+    width: '3'
+  },
+  {
+    label: '其他',
+    type: 'text',
+    name: 'ps',
+    value: '',
+    valid: '', //required
+    option: '',
+    width: '3'
+  }
+  ];
+
+
+  let rowIndex = ws.getLastRow()+1;
+  let colIndex;
+  /**
+   * 字串 轉 數字 parseInt() Number()
+   */
+  for (let i in stru) {
+    //寫資料
+    colIndex = parseInt(i) + 1;//
+    range = ws.getRange(rowIndex, colIndex);
+    range.setValue(e['parameter'][stru[i]['name']]);
+  }
+
+
+}
+
+/*=====================================
   20221222-5
 =====================================*/
 function test() {
