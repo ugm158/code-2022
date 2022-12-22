8df0937972681a40f013af38ff027f50e858d3d4
ugm158 <ugm158@gmail.com>
2022-12-22 13:45:14 +0800

    20221222-4-2

--- gas.js
+++ gas.js
@@ -1,4 +1,12 @@
 
+
+/*=====================================
+  20221222
+=====================================*/
+function test() {
   //取得試算表
   let ss = SpreadsheetApp.getActiveSpreadsheet();
-  console.log(ss);
+  //取得工作表
+  let ws = ss.getSheetByName('工作表7');
+  console.log(ws);
+}
