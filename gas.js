1b4039610762e01e206365abf4651096d12efdc6
ugm158 <ugm158@gmail.com>
2022-12-22 13:53:14 +0800

    20221222-4-3

--- gas.js
+++ gas.js
@@ -8,5 +8,10 @@ function test() {
   let ss = SpreadsheetApp.getActiveSpreadsheet();
   //取得工作表
   let ws = ss.getSheetByName('工作表7');
+  //取得範圍
+  let range = ws.getRange(1,1);
+
+  //寫資料
+
   console.log(ws);
 }
