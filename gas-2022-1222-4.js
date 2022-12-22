1e9dc633e1614f44cfd9dfc0f69473fce7656c2a
ugm158 <ugm158@gmail.com>
2022-12-22 14:09:32 +0800

    20221222-4-6

--- gas-2022-1222-4.js
+++ gas-2022-1222-4.js
@@ -1,4 +1,5 @@
 
+
 /*=====================================
   20221222
 =====================================*/
@@ -7,11 +8,24 @@ function test() {
   let ss = SpreadsheetApp.getActiveSpreadsheet();
   //取得工作表
   let ws = ss.getSheetByName('工作表7');
+
   //取得範圍
   let range = ws.getRange(1,1);
-
   //寫資料
   range.setValue('單位');
+
+  //寫資料
+  range = ws.getRange(1, 2);
+  range.setValue('日期');
+
+  //寫資料
+  range = ws.getRange(1, 3);
+  range.setValue('金額');
+
+  //寫資料
+  range = ws.getRange(1, 4);
+  range.setValue('其他');
+
 
   console.log(ws);
 }
