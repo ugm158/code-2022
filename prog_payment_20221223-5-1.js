c76dbb4cc965026ddada66fdab2cf862d44ed071
ugm158 <ugm158@gmail.com>
2022-12-23 14:43:05 +0800

    20221223-5-3

--- prog_payment_20221223-5-1.js
+++ prog_payment_20221223-5-1.js
@@ -47,39 +47,29 @@ function get_stru_payment() {
 /*=====================================
   繳費通知單 工作表
 =====================================*/
-function create_payment() {
+function create_payment(sheet='繳費通知單') {
   //取得試算表
   let ss = SpreadsheetApp.getActiveSpreadsheet();
   //取得工作表
-  let ws = ss.getSheetByName('工作表7');
+  let ws = ss.getSheetByName(sheet);
+  if(ws === null){//目前沒有工作表
+    ws = ss.insertSheet();
+    ws.setName(sheet);
+  }
   //取得範圍
   let range;
 
-  //整理資料 stru
-  //跑迴圈
   let stru_payment = get_stru_payment();
 
   let rowIndex = 1;
   let colIndex;
-  /**
-   * 字串 轉 數字 parseInt() Number()
-   */
+
   for (let i in stru_payment) {
     //寫資料
     colIndex = parseInt(i) + 1;//
     range = ws.getRange(rowIndex, colIndex);
     range.setValue(stru_payment[i]['label']);
   }
-
-
-  // for (let i = 0; i < data.length; i++) {
-  //   //寫資料
-  //   colIndex = i+1;
-  //   range = ws.getRange(rowIndex, colIndex);
-  //   range.setValue(data[i]);
-  // }
-
-
 }
 
 /*=====================================
---
+++ 程式碼.js
@@ -0,0 +1,21 @@
+
+/*=====================================
+ 安裝程式
+=====================================*/
+function setup() {
+  //-------------------------------------------- 把變數存入指令碼屬性
+  SCRIPT_PROP.setProperty("ssId", Sheet.getSs().getId());
+  SCRIPT_PROP.setProperty("adminEmail", Session.getActiveUser().getEmail());//管理員email
+
+  // 建立 全域變數
+  create_global();
+
+  // 商品類別
+  create_prod_kind();
+
+  // 繳費通知單
+  create_payment();
+
+
+
+}
