6bbac64a9fdc40df9133191c0850e36a138f461b
ugm158 <ugm158@gmail.com>
2022-12-23 14:21:17 +0800

    20221223-5-2

---
+++ prog_payment_20221223-5-1.js
@@ -0,0 +1,128 @@
+
+/*=====================================
+  取得 payment 結構
+=====================================*/
+function get_stru_payment() {
+  let stru = [{
+    label: '繳費單位',
+    type: 'select',
+    name: 'unit',
+    value: '',
+    valid: '', //required
+    option: '玉山|國泰|台新|電費|水費',
+    width: '3'
+  },
+  {
+    label: '繳費日期',
+    type: 'date',
+    name: 'date',
+    value: '',
+    valid: 'required', //required
+    option: '',
+    width: '3'
+  },
+  {
+    label: '繳費金額',
+    type: 'text',
+    name: 'money',
+    value: '',
+    valid: 'required', //required
+    option: '',
+    width: '3'
+  },
+  {
+    label: '其他',
+    type: 'text',
+    name: 'ps',
+    value: '',
+    valid: '', //required
+    option: '',
+    width: '3'
+  }
+  ];
+
+  return stru;
+}
+
+/*=====================================
+  繳費通知單 工作表
+=====================================*/
+function create_payment() {
+  //取得試算表
+  let ss = SpreadsheetApp.getActiveSpreadsheet();
+  //取得工作表
+  let ws = ss.getSheetByName('工作表7');
+  //取得範圍
+  let range;
+
+  //整理資料 stru
+  //跑迴圈
+  let stru_payment = get_stru_payment();
+
+  let rowIndex = 1;
+  let colIndex;
+  /**
+   * 字串 轉 數字 parseInt() Number()
+   */
+  for (let i in stru_payment) {
+    //寫資料
+    colIndex = parseInt(i) + 1;//
+    range = ws.getRange(rowIndex, colIndex);
+    range.setValue(stru_payment[i]['label']);
+  }
+
+
+  // for (let i = 0; i < data.length; i++) {
+  //   //寫資料
+  //   colIndex = i+1;
+  //   range = ws.getRange(rowIndex, colIndex);
+  //   range.setValue(data[i]);
+  // }
+
+
+}
+
+/*=====================================
+  繳費通知 主程式
+=====================================*/
+function payment(e) {
+  //取得試算表
+  let ss = SpreadsheetApp.getActiveSpreadsheet();
+  //取得工作表
+  let ws = ss.getSheetByName('工作表7');
+  //取得範圍
+  let range;
+
+  e = {
+    "parameter":
+    {
+      "date": "2022-12-25",
+      "money": "1000",
+      "ps": "1600",
+      "unit": "台新",
+      "callback_url":
+        "http://127.0.0.1:5500/form_20221222.html",
+      "op": "payment"
+    }
+  }
+
+  // console.log(e.parameter.unit);
+  // console.log(e['parameter']['unit']);
+
+  let stru_payment = get_stru_payment();
+
+
+  let rowIndex = ws.getLastRow() + 1;
+  let colIndex;
+  /**
+   * 字串 轉 數字 parseInt() Number()
+   */
+  for (let i in stru_payment) {
+    //寫資料
+    colIndex = parseInt(i) + 1;//
+    range = ws.getRange(rowIndex, colIndex);
+    range.setValue(e['parameter'][stru_payment[i]['name']]);
+  }
+
+
+}
