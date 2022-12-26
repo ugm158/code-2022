7b88f87d8775570d66df5fbc6daa03cf540b5b4d
ugm158 <ugm158@gmail.com>
2022-12-26 10:50:16 +0800

    20221226-2-3

--- prog_payment_20221226-2.js
+++ prog_payment_20221226-2.js
@@ -2,7 +2,14 @@ function test(){
   console.log(Route);
 
   Route.path("payment", payment);//
+  Route.path("payment1", payment1);//
 
   console.log(Route);
+
+}
+
+
+function payment1(e){
+  console.log("payment1");
 
 }
